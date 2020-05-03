658cdc793297f7f99907ec4e88efc45c25d8d57f
marcialfps <marcialfco@live.com>
2020-05-03 16:30:44 +0200

    Initial commit

---
+++ app/src/main/java/com/miw/skyscanner/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.miw.skyscanner
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
